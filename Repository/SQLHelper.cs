166b70db8fdd0fd2a78beddd5b6d0b5a975e9b26
James Alanis <fjam07@gmail.com>
2023-11-17 01:49:15 -0600

    Added student repo and helpers

---
+++ Repository/SQLHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace TEABackEndCodingChallenge;
+
+public static class SQLHelper
+{
+    private static readonly string _location;
+
+    static SQLHelper()
+    {
+        _location = Assembly.GetExecutingAssembly().Location;
+    }
+        
+    public static string GetSQLFromFile(this string fileName)
+    {
+        string currDir = Path.GetDirectoryName(_location);
+        string filePath = Path.Combine(currDir, "Repository", fileName);
+        return File.ReadAllText(filePath);
+    }
+}
---
+++ Repository/StudentRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using System.Data.Common;
+using Microsoft.Extensions.Caching.Memory;
+using NPoco;
+using TEABackEndCodingChallenge.Models;
+
+namespace TEABackEndCodingChallenge.Repository;
+
+public class StudentRepository : IStudentRepository
+{
+    private readonly IMemoryCache _cache;
+    private static readonly string _cacheName = "students";
+    private readonly INpgsqlConnection _dbConnection;
+    //private readonly ISqlConnection _dbConnection;
+
+    public StudentRepository(
+        IMemoryCache cache
+        ,INpgsqlConnection dbConnection
+        // ,ISqlConnection dbConnection
+    )
+    {
+        _cache = cache;
+        _dbConnection = dbConnection;        
+    }
+
+    public IEnumerable<Student> ReadAll()
+    {                                
+        var students = _cache.Get<IEnumerable<Student>>(_cacheName);
+
+        if (students is null)
+        {
+            try
+            {                
+                string sql = "StudentGrades".GetSQLFromFile();
+
+                using (IDatabase db = _dbConnection.NPocoDatabase)
+                {
+                    students = db.Fetch<Student>(sql);
+                }
+
+                _cache.Set(_cacheName, students, TimeSpan.FromMinutes(1));                
+            }
+            catch
+            {
+                throw;
+            }
+        }             
+
+        return students;
+    }
+}
