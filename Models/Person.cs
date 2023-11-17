306cdf57debd55e29af790e3f0d5b6a1abe2465f
James Alanis <fjam07@gmail.com>
2023-11-16 21:18:27 -0600

    Added models

---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using NPoco;
+
+namespace TEABackEndCodingChallenge.Models;
+
+[TableName("Person")]
+[PrimaryKey("PersonID")]
+public class Person
+{
+    public int PersonID { get; set; }
+
+    public string Discriminator { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public string Firstname { get; set; }
+    public DateTime HireDate { get; set; }
+    public string Lastname { get; set; }
+}
---
+++ Models/StudentGrade.cs
@@ -0,0 +1,14 @@
+﻿using NPoco;
+
+namespace TEABackEndCodingChallenge.Models;
+
+[TableName("StudentGrade")]
+[PrimaryKey("StudentID")]
+public class StudentGrade
+{
+    public int StudentID { get; set; }
+
+    public int CourseID { get; set; }
+    public int EnrollmentID { get; set; }
+    public decimal Grade { get; set; }    
+}
