ceb7f8ff2dc49b31175ae22b02a822d02b64713e
James Alanis <fjam07@gmail.com>
2023-11-17 01:49:39 -0600

    Added svc

---
+++ Services/IStudentService.cs
@@ -0,0 +1,9 @@
+﻿using TEABackEndCodingChallenge.Models;
+
+namespace TEABackEndCodingChallenge.Services;
+
+public interface IStudentService
+{
+    void CalculateGPA(IEnumerable<Student> students);
+    IEnumerable<Student> GetAll();
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,22 @@
+﻿using TEABackEndCodingChallenge.Models;
+
+namespace TEABackEndCodingChallenge.Services;
+
+public class StudentService : IStudentService
+{
+    private readonly IStudentRepository _studentRepo;
+
+    public StudentService(IStudentRepository studentRepo) => _studentRepo = studentRepo;
+
+    public void CalculateGPA(IEnumerable<Student> students)
+    {
+        foreach (Student student in students.Where(s => s.Grade > 0))
+        {
+            decimal gradesSum = students.Where(s => s.StudentID == student.StudentID).Sum(g => g.Grade);
+            decimal gradeCount = students.Where(s => s.StudentID == student.StudentID && s.Grade > 0).Count();
+            student.GPA = gradesSum / gradeCount;
+        }   
+    }    
+
+    public IEnumerable<Student> GetAll() => _studentRepo.ReadAll();
+}
